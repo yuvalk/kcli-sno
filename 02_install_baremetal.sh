2b272aabd86be16078fba9eb973ff0192d8d2b07
Yuval Kashtan <yuvalkashtan@gmail.com>
2021-11-20 22:24:46 +0200

    actually install baremetal machine with sno iso
    this uses the racadm.sh helper to connect through the bmc and do the
    following:
    1. connect the iso as virtual media
    2. set boot once from virtual cd/dvd
    3. power cycle the machine
    
    this will boot the bare metal node with the live iso
    which will initiate installation of OCP SNO

---
+++ 02_install_baremetal.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+racadm.sh {{ cluster }} {{ sno_bmc_ip }} {{ sno_bmc_username }} {{ sno_bmc_password }}
--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ echo "********* RUNNING 01_create_iso.sh *********"
 bash /root/01_create_iso.sh
 
 echo "********* RUNNING 02_redfish.sh *********"
-bash /root/02_redfish.sh
+bash /root/02_install_baremetal.sh
+
