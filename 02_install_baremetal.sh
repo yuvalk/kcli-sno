acd47ef28a30d2e176de2784abe09d2d6883aa5a
Yuval Kashtan <yuvalkashtan@gmail.com>
2021-11-21 12:57:09 +0200

    fix script paths
    since these are run from cloud-init, need full path to files and scripts
    they are calling

--- 02_install_baremetal.sh
+++ 02_install_baremetal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-racadm.sh {{ cluster }} {{ sno_bmc_ip }} {{ sno_bmc_username }} {{ sno_bmc_password }}
+/root/bin/racadm.sh {{ cluster }} {{ sno_bmc_ip }} {{ sno_bmc_username }} {{ sno_bmc_password }}
