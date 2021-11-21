9d75d41fc85f0352c0b065683f1132ebcc214c4a
Yuval Kashtan <yuvalkashtan@gmail.com>
2021-11-21 13:20:27 +0200

    add racadm dnf installation
    this is using the official dell yum repo for RHEL8

--- 00_prepare.sh
+++ 00_prepare.sh
@@ -10,6 +10,13 @@ systemctl enable --now libvirtd
 dnf -y copr enable karmab/kcli ; dnf -y install kcli
 dnf -y install httpd
 
+echo """[racadm]
+name=Racadm
+baseurl=http://linux.dell.com/repo/hardware/dsu/os_dependent/RHEL8_64
+enabled=1
+gpgcheck=0""" > /etc/yum.repos.d/racadm.repo
+dnf -y install openssl-devel srvadmin-idracadm7
+
 systemctl enable --now httpd
 kcli create host kvm -H 127.0.0.1 local
 kcli create pool -p /var/www/html default
