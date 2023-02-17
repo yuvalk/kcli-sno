cfd01e97d7d41dc2c227cb8d6ff1c89e091d3c2b
karmab <karimboumedhel@gmail.com>
2023-02-17 14:08:19 +0100

    use redfish library to boot host

--- 02_install_baremetal.sh
+++ 02_install_baremetal.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-/root/bin/racadm.sh {{ cluster }} {{ sno_bmc_ip }} {{ sno_bmc_username }} {{ sno_bmc_password }}
+CLUSTER={{ cluster }}
+IP={{ bmc_ip }}
+MODEL={{ bmc_model }}
+USER={{ bmc_username }}
+PASSWORD={{ bmc_password }}
+ISO_URL="http://$(hostname -I | cut -f1 -d" " | xargs)/$CLUSTER-sno.iso"
+kcli start baremetal-host -P iso_url=$ISO_URL -P url=$IP -P model=$MODEL -P user=$USER -p password=$PASSWORD
---
+++ kcli_pre.sh
@@ -0,0 +1,3 @@
+{% if bmc_ip == None %}
+echo bmc_ip needs to be set && exit 1
+{ %endif %}
